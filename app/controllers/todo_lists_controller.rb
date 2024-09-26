f807aa2b2f7071e893a264b5d7b78ca94c168eb8
Gaston Ciancio <gaston.ciancio@snappler.com>
2024-09-26 18:50:36 -0300

    finish app

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,15 +1,68 @@
 class TodoListsController < ApplicationController
-  # GET /todolists
+  before_action :find_list, only: %i[update edit destroy show]
   def index
     @todo_lists = TodoList.all
 
     respond_to :html
   end
 
-  # GET /todolists/new
   def new
     @todo_list = TodoList.new
 
     respond_to :html
+  end
+
+  def create
+    @todo_list = TodoList.new(todo_list_params)
+
+    if @todo_list.save
+      handle_successful_create
+    else
+      handle_failed_create
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @todo_list.update(todo_list_params)
+      redirect_to todo_lists_path
+    else
+      handle_failed_update
+    end
+  end
+
+  def destroy
+    @todo_list.destroy
+    render turbo_stream: [turbo_stream.remove(@todo_list)]
+  end
+
+  private
+
+  def todo_list_params
+    params.require(:todo_list).permit(:name)
+  end
+
+  def find_list
+    @todo_list = TodoList.find(params[:id])
+  end
+
+  def handle_successful_create
+    render turbo_stream: [
+      turbo_stream.update('new_todolist', partial: 'todo_lists/new', locals: { todo_list: TodoList.new }),
+      turbo_stream.append('added_new_todolist', partial: 'todo_lists/todolist', locals: { todo_list: @todo_list })
+    ]
+  end
+
+  def handle_failed_create
+    render turbo_stream: [
+      turbo_stream.update('create_todolist_error'.to_sym, partial: '/feedback', locals: { messages: @todo_list.errors.messages })
+    ], status: :unprocessable_entity
+  end
+
+  def handle_failed_update
+    render turbo_stream: [
+      turbo_stream.update('update_todolist_error'.to_sym, partial: '/feedback', locals: { messages: @todo_list.errors.messages })
+    ], status: :unprocessable_entity
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :todo_lists, only: %i[index create update destroy], path: :todolists
-    resources :todo_items, only: %i[index create update destroy], path: :todoitems
-
+    resources :todo_lists, only: %i[index create update destroy]
+    resources :todo_items, only: %i[index create update destroy]
   end
 
-  resources :todo_lists, only: %i[index new], path: :todolists
+  resources :todo_lists, only: %i[index new create edit update destroy] do
+    resources :todo_items, only: %i[index]
+  end
+
+  resources :todo_items, only: %i[new create edit update destroy]
+
+  root to: redirect('/todo_lists')
 end
